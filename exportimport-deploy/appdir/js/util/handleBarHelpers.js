f2e7ac55a95fb93d3dc79be5914af6fea0845d48
samueldoyle <samueldoyle@vmware.com>
2013-10-16 20:39:51 -0700

    Fix optimized build
    Testing Done:
    Issue Number:
    Approved by:
    Reviewed by:

--- exportimport-deploy/appdir/js/util/handleBarHelpers.js
+++ exportimport-deploy/appdir/js/util/handleBarHelpers.js
@@ -1,13 +1,1 @@
-/**
- * If you have any handlebar helpers can place them here
- * @author samueldoyle
- */
-define(function (require) {
-    var Handlebars = require("Handlebars");
-    assert(Handlebars, "Unable to find handlebars dependency");
-
-    // {{year}}
-    Handlebars.registerHelper("yearHelper", function() {
-        return new Date().getFullYear();
-    });
-});
+define(function(require){var Handlebars=require("Handlebars");assert(Handlebars,"Unable to find handlebars dependency"),Handlebars.registerHelper("yearHelper",function(){return(new Date).getFullYear()})});
